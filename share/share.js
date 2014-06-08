f2dbf049396d565f33b94476a85348b357758eb6
Matt Mastracci <MattMastracci@smarttech.com>
2014-06-07 19:07:54 -0600

    More tweaks to this sharing

--- share/share.js
+++ share/share.js
@@ -1,24 +1,11 @@
 var addthis_config = { "data_track_addressbar": false, "data_track_clickback": false, "ui_508_compliant": true };
-/*
-var createElement = document.createElement;
 
-document.createElement = function(tagName) {
-	var elem = createElement.apply(document, arguments);
-	console.log(tagName);
-	if (tagName == "script" || tagName == "iframe") {
-		elem.__defineSetter__('src', function(value) {
-			console.log(value);
-			if (value.slice(0, 5) == "http:") {
-				value = 'https:' + value.slice(6);
-			}
-			if (value.slice(0, 2) == "//") {
-				value = 'https:' + value;
-			}
-			console.log(value);
-			elem.setAttribute('src', value);
-		});
-	}
-
-	return elem;
+if (!window.addthis) {
+    setTimeout(function() { populatePopup(id) }, 100);
+    return;
 }
-*/
+
+var loading = document.querySelectorAll('.addthis_loading');
+for (var i = 0; i < loading.length; i++) {
+    loading[i].style.display = 'none';
+}
